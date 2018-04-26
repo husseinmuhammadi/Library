23b00ccc6ebbdb8f07bf8a2f674626acd0a89548
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-04-26 21:57:49 +0430

    Remove unnecessary classes

--- model/src/main/java/com/npci/isc/model/to/ReconciliationTransaction.java
+++
@@ -1,9 +0,0 @@
-package com.npci.isc.model.to;
-
-import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-
-public abstract class ReconciliationTransaction {
-
-}
--- model/src/main/java/com/npci/isc/model/to/Reversal.java
+++
@@ -1,7 +0,0 @@
-package com.npci.isc.model.to;
-
-import javax.persistence.Entity;
-
-public abstract class Reversal {
-
-}
