087e89405a96d6d27f55572a3e33d59cc2917d4d
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-03 23:27:04 +0430

    Generate book id based on sequence generated value

--- model/src/main/java/com/npci/lms/library/model/to/Book.java
+++ model/src/main/java/com/npci/lms/library/model/to/Book.java
@@ -1,11 +1,14 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Book {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
