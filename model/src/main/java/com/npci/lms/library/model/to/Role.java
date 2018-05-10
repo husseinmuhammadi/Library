92a48f125e5d10d76cc33fd174623c1573ef9dd4
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 16:52:50 +0430

    Role

--- model/src/main/java/com/npci/lms/library/model/to/Role.java
+++ model/src/main/java/com/npci/lms/library/model/to/Role.java
@@ -1,38 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-@Entity(name = "LROLE")
-public class Role {
-    @Id
-    private Long id;
-
-    private String role;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Entity
+@Table(name = "ROLE")
+@SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "ROLE_SEQ")
+public class Role extends EntityBase {
 }
