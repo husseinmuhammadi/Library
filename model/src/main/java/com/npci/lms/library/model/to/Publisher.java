ef5edc1d9cb205823968a5f20f027dc0a6b536cb
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 16:41:08 +0430

    Publisher

--- model/src/main/java/com/npci/lms/library/model/to/Publisher.java
+++ model/src/main/java/com/npci/lms/library/model/to/Publisher.java
@@ -1,39 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
-public class Publisher {
-
-    @Id
-    private Long id;
-
-    private String name;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Table(name = "PUBLISHER")
+@SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "PUBLISHER_SEQ")
+public class Publisher extends EntityBase {
 }
