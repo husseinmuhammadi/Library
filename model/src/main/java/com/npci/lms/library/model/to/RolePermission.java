0704d4f7c0c416245224c13bd99a831415bd33a1
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 17:08:04 +0430

    Role permission

--- model/src/main/java/com/npci/lms/library/model/to/RolePermission.java
+++ model/src/main/java/com/npci/lms/library/model/to/RolePermission.java
@@ -1,39 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
-public class RolePermission {
-
-    @Id
-    private Long id;
-
-    private String permission;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPermission() {
-        return permission;
-    }
-
-    public void setPermission(String permission) {
-        this.permission = permission;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Table(name = "ROLE_PERMISSION")
+@SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "ROLE_PERMISSION_SEQ")
+public class RolePermission extends EntityBase {
 }
