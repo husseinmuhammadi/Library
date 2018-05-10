0c210407d689a36c50651323ecc0dd0e88797285
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 17:09:41 +0430

    User account (table object)

--- model/src/main/java/com/npci/lms/library/model/to/UserAccount.java
+++ model/src/main/java/com/npci/lms/library/model/to/UserAccount.java
@@ -1,39 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
-public class UserAccount {
-
-    @Id
-    private Long id;
-
-    private String account;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Table(name = "USER_ACCOUNT")
+@SequenceGenerator(name = "", sequenceName = "USER_ACCOUNT_SEQ")
+public class UserAccount extends EntityBase {
 }
