bac88734c5b315267f1bca0817bc31b810ded098
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 16:40:18 +0430

    Book history

--- model/src/main/java/com/npci/lms/library/model/to/BookHistory.java
+++ model/src/main/java/com/npci/lms/library/model/to/BookHistory.java
@@ -1,39 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
-public class BookHistory {
-
-    @Id
-    private Long id;
-
-    private String history;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getHistory() {
-        return history;
-    }
-
-    public void setHistory(String history) {
-        this.history = history;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Table(name = "BOOK_HISTORY")
+@SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "BOOK_HISTORY_SEQ")
+public class BookHistory extends EntityBase {
 }
