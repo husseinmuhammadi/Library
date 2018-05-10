9f2de9196710a8eae0e52560562536bb0800badc
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 17:19:07 +0430

    User role (table object)

--- model/src/main/java/com/npci/lms/library/model/to/UserRole.java
+++ model/src/main/java/com/npci/lms/library/model/to/UserRole.java
@@ -1,39 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
-public class UserRole {
-
-    @Id
-    private  Long id;
-
-    private String role;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+@Table(name = "USER_ROLE")
+@SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "USER_ROLE_SEQ")
+public class UserRole extends EntityBase {
 }
