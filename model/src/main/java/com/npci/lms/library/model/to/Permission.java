f803e02921796c7823f6ca12b24d0f3bbad83fec
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-05-10 18:41:40 +0430

    Permission (table object)

--- model/src/main/java/com/npci/lms/library/model/to/Permission.java
+++ model/src/main/java/com/npci/lms/library/model/to/Permission.java
@@ -1,39 +1,11 @@
 package com.npci.lms.library.model.to;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-@Entity(name = "LPERMISSION")
-public class Permission {
-    @Id
-    private Long id;
-
-    private String name;
-
-    private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
+@Entity
+@Table(name = "PERMISSION")
+@SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "PERMISSION_SEQ")
+public class Permission extends EntityBase  {
 }
