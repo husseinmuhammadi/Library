e7142b2e0168d4b008f24b441df874460893a94b
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2018-05-30 20:09:07 +0430

    Template and menu

---
+++ web/src/main/java/com/npci/lms/library/web/controller/TemplateController.java
@@ -0,0 +1,46 @@
+package com.npci.lms.library.web.controller;
+
+import com.npci.lms.library.web.controller.user.UserInformation;
+import org.apache.shiro.SecurityUtils;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Locale;
+
+@Named
+@SessionScoped
+public class TemplateController implements Serializable {
+
+    @Inject
+    private UserInformation userInformation;
+
+    private String localeValue = "en_US";
+
+    private boolean administrator;
+
+    public String getLocaleValue() {
+        administrator = SecurityUtils.getSubject().hasRole("administrator");
+        return localeValue;
+    }
+
+    @PostConstruct
+    private void init() {
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("en", "US"));
+    }
+
+    public UserInformation getUserInformation() {
+        return userInformation;
+    }
+
+    public void setUserInformation(UserInformation userInformation) {
+        this.userInformation = userInformation;
+    }
+
+    public boolean isAdministrator() {
+        return administrator;
+    }
+}
